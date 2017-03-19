879e6cb726bdb08a72976b62119d99579f97dbc7
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2017-03-19 10:08:15 -0400

    more bandstand tests

--- JazzQuintetComboKata/src/main/scala/todd/jazzQuintet/Bandstand.scala
+++ JazzQuintetComboKata/src/main/scala/todd/jazzQuintet/Bandstand.scala
@@ -1,6 +1,12 @@
 package todd.jazzQuintet
 
 class Bandstand(musiciansIn: Seq[Musician]) {
-  require(musiciansIn.size == 5, "Bandstand must be a quintet of 5 musicians.  Found " + musiciansIn.size)
+  validateIsProperQuintet
   val musicians = musiciansIn.sortBy { _.instrument.soloOrder }
+  
+  def validateIsProperQuintet = {
+    require(musiciansIn.size == 5, "Bandstand must be a quintet of 5 musicians.  Found " + musiciansIn.size)
+    require(musiciansIn.map (_.instrument).toSet.size == 5)
+  }
 }
+
--- JazzQuintetComboKata/src/test/scala/todd/jazzQuintet/BandstandTest.scala
+++ JazzQuintetComboKata/src/test/scala/todd/jazzQuintet/BandstandTest.scala
@@ -4,7 +4,7 @@ import org.scalatest._
 class BandstandTest extends FunSpec with ShouldMatchers {
   
   def quintet = it
-  def jazzCats = new Bandstand(Seq(
+  val jazzCats = new Bandstand(Seq(
       Musician("Paul Chambers", Bass, null),
       Musician("John Coletrane", Saxophone, null),
       Musician("Bill Evans", Piano, null),
@@ -34,6 +34,26 @@ class BandstandTest extends FunSpec with ShouldMatchers {
     ))
   }
   
-  // TODO: exception tests for too many, too few, and wrong instrument mix
+  quintet("is not a trio") {
+      intercept[IllegalArgumentException] {
+        val trio = new Bandstand(Seq(
+        Musician("Paul Chambers", Bass, null),
+        Musician("John Coletrane", Saxophone, null),
+        Musician("Jimmy Cobb", Drums, null)
+        ))
+      }
+  }
+  
+  quintet("has the proper mix of instruments") {
+      intercept[IllegalArgumentException] {
+        val saxBand = new Bandstand(Seq(
+            Musician("Paul Chambers", Bass, null),
+            Musician("John Coletrane", Saxophone, null),
+            Musician("Bill Evans", Piano, null),
+            Musician("Wayne Shorter", Saxophone, null),
+            Musician("Jimmy Cobb", Drums, null)
+        ))
+      }
+  }
   
 }
