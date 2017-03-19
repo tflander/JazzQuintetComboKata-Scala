7476203beb4158b6c580cb0907f10d354df87384
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2017-03-18 22:04:02 -0400

    wait before suggesting a song

--- JazzQuintetComboKata/src/main/scala/todd/jazzQuintet/Musician.scala
+++ JazzQuintetComboKata/src/main/scala/todd/jazzQuintet/Musician.scala
@@ -7,6 +7,17 @@ class Musician(songs: Seq[String]) {
   private val random = new Random(new Date().getTime)
   
   def songSuggestion = songs(random.nextInt(songs.size))
+  
+  def suggestSongIfNoOneElseDoes = {
+    val delay = random.nextInt(200) + 100
+    Thread.sleep(delay)
+    
+    // TODO: check for a song suggestion from someone else
+    //       if someone else suggests, then return None
+    
+    Some(songSuggestion)
+  }
+  
   def knows(song: String) = songs.contains(song)
 }
 
--- JazzQuintetComboKata/src/test/scala/todd/jazzQuintet/MusicianTest.scala
+++ JazzQuintetComboKata/src/test/scala/todd/jazzQuintet/MusicianTest.scala
@@ -1,5 +1,6 @@
 package todd.jazzQuintet
 import org.scalatest._
+import java.util.Date
 
 class MusicianTest extends FunSpec with ShouldMatchers {
   
@@ -11,25 +12,33 @@ class MusicianTest extends FunSpec with ShouldMatchers {
   
   val bassist = Musician(songs)
   
-  def theMusician = it
+  def musician = it
   
   describe("song suggestion tests") {
-    theMusician("suggests a random song") {
+    
+    musician("recalls a random song") {
       songs should contain (bassist songSuggestion)
     }
     
-    theMusician("eventually suggests every song") {
+    musician("eventually suggests every song") {
       val suggestedSongs = (for (a <- 1 to 100) yield bassist songSuggestion)
         .toSet
       suggestedSongs should be(songs.toSet)
     }
     
-    theMusician("agrees to play a song he knows") {
+    musician("agrees to play a song he knows") {
       bassist knows "song b" should be(true)
     }
     
-    theMusician("does not agree to play a song he doesnt know") {
+    musician("does not agree to play a song he doesnt know") {
       bassist knows "smoke on the water" should be(false)
+    }
+    
+    musician ("waits patiently before suggesting a song") {
+      val start = new Date().getTime
+      bassist suggestSongIfNoOneElseDoes
+      val elapsed = new Date().getTime - start
+      elapsed should (be > 100L and be < 300L)
     }
   }
 }
